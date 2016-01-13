95fcc150481d288e94f8a0ea0e9d242b1d61049e
王王 <wangwang@wangwangdemacbook-pro.local>
2016-01-13 13:43:31 +0800

    Initial Commit

---
+++ YXArcView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXArcView
+//
+//  Created by 王王 on 16/1/13.
+//  Copyright © 2016年 王王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXArcView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXArcView
+//
+//  Created by 王王 on 16/1/13.
+//  Copyright © 2016年 王王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
