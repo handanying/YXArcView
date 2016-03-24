4b80c99b3c602a900cd34a9d7d967c3f150fe905
wangwang <handanying@126.com>
2016-03-24 11:28:53 +0800

    删除注释
    删除注释

--- YXArcView/ArcView.m
+++ YXArcView/ArcView.m
@@ -43,7 +43,6 @@
 
 - (instancetype)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
-    // 新人报到
     // 此处有潜在的内存泄露
     if (self) {
         self.backgroundColor = [UIColor whiteColor];
--- YXArcView/ViewController.m
+++ YXArcView/ViewController.m
@@ -14,7 +14,6 @@
 @end
 
 @implementation ViewController
-// 增加注释
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
